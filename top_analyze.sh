2c5003269008ae284fa4df761fdc5052380d55c6
leapking <leapking@126.com>
2019-02-23 19:03:56 +0800

    chang delimiter to \t

--- top_analyze.sh
+++ top_analyze.sh
@@ -19,15 +19,15 @@ elif ! grep -c top $topfile >/dev/null; then
 fi
 
 if [ x"$type" = "xmem" ]; then
-	echo "TIME,VIRT,RES,%MEM"
+        echo "TIME\tVIRT\tRES\t%MEM"
         awk 'function m2gb(size){if(size~/m/){gsub(/m/,"",size);size=sprintf("%.1f",size/1024)}return size} \
-	BEGIN{info=""}/^top/||/'"$program"'\s*$/{if(/^top/){info=$3;}else if(/'"$program"'/){info=info","m2gb($5)","m2gb($6)","$10;gsub(/[%g]/,"",info);print info;info=""}}' $topfile
+        BEGIN{info=""}/^top/||/'"$program"'\s*$/{if(/^top/){info=$3;}else if(/'"$program"'/){info=info"\t"m2gb($5)"\t"m2gb($6)"\t"$10;gsub(/[%g]/,"",info);print info;info=""}}' $topfile
 elif [ x"$type" = "xcpu" ]; then
-	echo "time,%user,%system,%idle"
-	awk 'BEGIN{info=""}/^top|Cpu\(s\)/{if($1=="top"){info=$3;}else if(/Cpu\(s\)/){info=info","$2$3$5;gsub(/[A-Za-Z%]/,"",info);print info;info=""}}' $topfile
+        echo "time\t%user\t%system\t%idle"
+        awk -F '[, ]+' 'BEGIN{info=""}/^top|Cpu\(s\)/{if($1~/top/){info=$3;}else if(/Cpu\(s\)/){info=info"\t"$2"\t"$3"\t"$5;gsub(/[A-Za-z%,]/,"",info);print info;info=""}}' $topfile
 elif [ x"$type" = "xload" ]; then
-	echo "time, average, max, min"
-	awk '/^top/{print $3", "$12" "$13" "$14}' $topfile
+        echo "time\taverage\tmax\tmin"
+        awk '/^top/{time=$3;gsub(/^.*load average:/,"",$0);gsub(/,/,"",$0);print time"\t"$1"\t"$2"\t"$3}' $topfile
 else
 	echo "Unknown type: $type"
 fi
