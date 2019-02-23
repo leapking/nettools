f3da7dfa84195b200c5258cbfcde76998dbeea72
leapking <leapking@126.com>
2019-02-23 19:02:50 +0800

    convert MB size of memory to GB

--- top_analyze.sh
+++ top_analyze.sh
@@ -20,7 +20,8 @@ fi
 
 if [ x"$type" = "xmem" ]; then
 	echo "TIME,VIRT,RES,%MEM"
-	awk 'BEGIN{info=""}/^top/||/'"$program"'\s*$/{if(/^top/){info=$3;}else if(/'"$program"'/){info=info","$5","$6","$10;gsub(/[A-Za-Z]/,"",info);print info;info=""}}' $topfile
+        awk 'function m2gb(size){if(size~/m/){gsub(/m/,"",size);size=sprintf("%.1f",size/1024)}return size} \
+	BEGIN{info=""}/^top/||/'"$program"'\s*$/{if(/^top/){info=$3;}else if(/'"$program"'/){info=info","m2gb($5)","m2gb($6)","$10;gsub(/[%g]/,"",info);print info;info=""}}' $topfile
 elif [ x"$type" = "xcpu" ]; then
 	echo "time,%user,%system,%idle"
 	awk 'BEGIN{info=""}/^top|Cpu\(s\)/{if($1=="top"){info=$3;}else if(/Cpu\(s\)/){info=info","$2$3$5;gsub(/[A-Za-Z%]/,"",info);print info;info=""}}' $topfile
